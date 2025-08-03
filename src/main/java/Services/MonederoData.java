527ad8b1ae131607e91f7f565b711b6f1da9849a
Sergio Gomez <sergioferg.2003@gmail.com>
2025-08-03 12:30:19 -0400

    Listo reconocimiento facial y cobro de tarifas, falta que se muestre en el historial

--- src/main/java/Services/MonederoData.java
+++ src/main/java/Services/MonederoData.java
@@ -47,19 +47,18 @@ public class MonederoData {
             for(int i = 0; i < contenido.length - 1; i++) {
                 writer.write(contenido[i] + ",");
             }
-            String[] recargas = contenido[contenido.length-1].split("\n");
+            String[] recargas = contenido[contenido.length-1].split("#");
             for(String i : recargas) {
                 System.err.println(i);
             }
             System.err.println(recargas.length);
-            for(int i = 0; i < recargas.length; i++) {
-                writer.write(recargas[i]);
+            for(int i = 0; i < recargas.length - 1; i++) {
+                writer.write(recargas[i] + " #");
                 writer.newLine();
-                writer.write("\\n");
             }
             
             String fecha = "[" + new SimpleDateFormat("dd/MM/yyyy HH:mm").format(new Date()) + "]";
-            writer.write(fecha + " Visita al comedor: $-" + ccb);
+            writer.write("\\n " + fecha + " Visita al comedor: $-" + ccb);
             writer.newLine();
             writer.write("\\n");
             
